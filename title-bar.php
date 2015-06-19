0e92de9a450e14c333856212f33d2bb24ad0482c
David Chandra Purnama <david@shellcreeper.com>
2015-06-19 22:35:01 +0700

    fix title bar markup

--- title-bar.php
+++ title-bar.php
@@ -6,21 +6,35 @@
  * Singular: Post type.
  * @since 1.0.0
 **/
-$title = get_bloginfo( 'name' );
 if ( hybrid_get_loop_title() ){
 	$title = hybrid_get_loop_title();
+	?>
+	<header itemtype="http://schema.org/WebPageElement" itemscope="itemscope" class="loop-meta">
+		<h1 itemprop="headline" class="loop-title"><?php echo $title; ?></h1>
+	</header>
+	<?php
+}
+elseif( is_front_page() && is_home() ){
+	$title = __( 'Blog', 'daftar' );
+	?>
+	<div class="loop-meta">
+		<div class="loop-title"><?php echo $title; ?></div>
+	</div>
+	<?php
 }
 elseif( is_singular() ){
 	$title = get_post_type_labels( get_post_type_object( get_post_type( get_queried_object_id() ) ) )->singular_name;
+	?>
+	<div class="loop-meta">
+		<div class="loop-title"><?php echo $title; ?></div>
+	</div>
+	<?php
 }
-if( is_front_page() && is_home() ){
-	$title = __( 'Blog', 'daftar' );
+else{
+	$title = get_bloginfo( 'name' );
+	?>
+	<div class="loop-meta">
+		<div class="loop-title"><?php echo $title; ?></div>
+	</div>
+	<?php
 }
-?>
-
-<header itemtype="http://schema.org/WebPageElement" itemscope="itemscope" class="loop-meta">
-	<h1 itemprop="headline" class="loop-title"><?php echo $title; ?></h1>
-</header>
-
-
-

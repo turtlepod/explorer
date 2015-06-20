51035301679711d0c9c91411b5f361b3408ae978
David Chandra Purnama <david@shellcreeper.com>
2015-06-20 13:36:42 +0700

    Add page template to display navigation menu as landing page.

--- functions.php
+++ functions.php
@@ -38,6 +38,12 @@ function explorer_setup(){
 	);
 	add_theme_support( 'tamatebako-sidebars', $sidebars_args );
 
+	/* === Register Menus === */
+	$menus_args = array(
+		"home" => _x( 'Home Navigation', 'nav menu', 'explorer' ),
+	);
+	add_theme_support( 'tamatebako-menus', $menus_args );
+
 	/* === Load Stylesheet === */
 	$style_args = array(
 		'theme-open-sans-font',
@@ -146,6 +152,45 @@ function explorer_sanitize_checkbox( $input ){
 		return true;
 	}
 	return false;
+}
+
+/**
+ * Home Navigation Walker
+ * @link http://wpbeg.in/V8Sa8i
+ * @since 1.0.0
+ */
+class Explorer_Home_Navigation extends Walker_Nav_Menu {
+
+	function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
+		global $wp_query;
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+		$class_names = $value = '';
+
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
+		$class_names = ' class="' . esc_attr( $class_names ) . '"';
+
+		$output .= $indent . '<li id="menu-item-'. $item->ID . '"' . $value . $class_names .'>';
+
+		$attributes = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) .'"' : '';
+		$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) .'"' : '';
+		$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) .'"' : '';
+		$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) .'"' : '';
+		$wrap_class = ! empty( $item->xfn ) ? ' rel-' . esc_attr( $item->xfn ) : '';
+
+		$item_output = $args->before;
+		$item_output .= '<div class="home-menu-item-wrap' . $wrap_class . '">';
+		$item_output .= '<a'. $attributes .'>';
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= '<br /><span class="menu-item-desc">' . $item->description . '</span>';
+		$item_output .= '</div>';
+		$item_output .= $args->after;
+
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
 }
 
 
